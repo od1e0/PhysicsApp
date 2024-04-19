01054d2be10cf5279ed3eaa02e5a1a8f737cf91b
Vladimir Zditovec <vladimir.zdit11@gmail.com>
2024-04-19 03:32:03 +0300

    Create Views and Styles

--- PhysicsApp/ViewModels/ViewModelBase.cs
+++ PhysicsApp/ViewModels/ViewModelBase.cs
@@ -10,7 +10,6 @@ namespace PhysicsApp.ViewModels
     public class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-
         public void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
